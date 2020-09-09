e045a91e56d4cbb4c4f57756a89b2e51010509c3
Santiago Agudelo <sagudelo1200@gmail.com>
2020-09-09 19:51:44 +0000

    stats added

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,9 +4,21 @@
 
 from flask import jsonify
 from api.v1.views import app_views
+from models import storage
+
+classes = {"Amenity": "amenities", "City": "cities",
+           "Place": "places", "Review": "reviews",
+           "State": "states", "User": "users"}
 
 
-@app_views.route('/status', methods=['GET'])
+@app_views.route('/status')
 def status():
     """ return status """
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats')
+def stats():
+    """ data stats """
+    stats = dict([(val, storage.count(cls)) for cls, val in classes.items()])
+    return jsonify(stats)
