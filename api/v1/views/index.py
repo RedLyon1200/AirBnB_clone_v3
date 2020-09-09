839c22e2926e02935a88803b981028f24996e52a
Santiago Agudelo <sagudelo1200@gmail.com>
2020-09-09 19:27:18 +0000

    Testing PUSHER - api/v1/views/index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,10 +2,11 @@
 """
 """
 
+from flask import jsonify
 from api.v1.views import app_views
-from flask import jsonify 
 
-@app_views.route('/status', strict_slashes=False)
+
+@app_views.route('/status', methods=['GET'])
 def status():
     """ return status """
     return jsonify({"status": "OK"})
