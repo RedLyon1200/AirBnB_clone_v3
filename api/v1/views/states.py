8355d881bbb3d33a65864e83d9a65e2ac22400d6
Santiago Agudelo <sagudelo1200@gmail.com>
2020-09-10 01:01:20 +0000

    methods PUT and POST added

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -2,10 +2,12 @@
 """
 """
 
-from flask import jsonify, abort
+from flask import jsonify, abort, request
 from api.v1.views import app_views
 from models import storage
 from models.state import State
+
+
 
 
 @app_views.route('/states/<state_id>', methods=['GET'])
@@ -38,3 +40,42 @@ def delete_state(state_id):
     storage.save()
 
     return {}, 200
+
+
+@app_views.route('/states', methods=['POST'])
+def create_state():
+    """ Creates a State """
+    try:
+        data = request.get_json()
+    except Exception:
+        abort(400, description='Not a JSON')
+
+    if not data.get('name'):
+        abort(400, description='Missing name')
+
+    new_state = State(**data)
+    new_state.save()
+    return jsonify(new_state.to_dict()), 201
+
+
+@app_views.route('/states/<state_id>', methods=['PUT'])
+def put_state(state_id):
+    """ updates state """
+    state = storage.get(State, state_id)
+
+    if not state:
+        abort(404)
+
+    try:
+        data = request.get_json()
+    except Exception:
+        abort(400, description='Not a JSON')
+
+    for key, val in data.items():
+        ignore_data = ['id', 'created_at', 'updated_at']
+
+        if key not in ignore_data:
+            setattr(state, key, val)
+
+    storage.save()
+    return jsonify(state.to_dict()), 200
