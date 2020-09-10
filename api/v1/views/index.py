e36a16094b63af617cdc1886244ebfdd707d0b92
Santiago Agudelo <sagudelo1200@gmail.com>
2020-09-10 19:17:11 +0000

    strict slashed adden in index

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -6,18 +6,20 @@ from flask import jsonify
 from api.v1.views import app_views
 from models import storage
 
+opt_route = {'strict_slashes': False}
+
 classes = {"Amenity": "amenities", "City": "cities",
            "Place": "places", "Review": "reviews",
            "State": "states", "User": "users"}
 
 
-@app_views.route('/status')
+@app_views.route('/status', **opt_route)
 def status():
     """ return status """
     return jsonify({"status": "OK"})
 
 
-@app_views.route('/stats')
+@app_views.route('/stats', **opt_route)
 def stats():
     """ data stats """
     stats = dict([(val, storage.count(cls)) for cls, val in classes.items()])
